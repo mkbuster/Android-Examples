53d765f50ee7b23b79cd083f8fcee08d4044b898
Max Buster <max@outway.io>
2020-10-08 18:02:28 -0700

    Initial Commit

---
+++ app/src/test/java/com/max/examples/CoroutineTest.kt
@@ -0,0 +1,4 @@
+package com.max.examples
+
+class CoroutineTest {
+}
---
+++ app/src/test/java/com/max/examples/FlowTest.kt
@@ -0,0 +1,14 @@
+package com.max.examples
+
+import kotlinx.coroutines.flow.flow
+import org.junit.Test
+
+class FlowTest {
+
+    @Test
+    fun example1() {
+        flow<Int> {
+
+        }
+    }
+}
